b5e9d1d491f880322be28caa013ef519f04d332a
bejesh-git <bejeshsriram2808@gmail.com>
2025-01-15 17:45:07 +0530

    backend

--- Back-End/AttendanceTask/Controllers/AdminController.cs
+++ Back-End/AttendanceTask/Controllers/AdminController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AttendanceTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AttendanceTask.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+
+        private readonly AttendanceDbContext _context;
+
+
+        public AdminController(AttendanceDbContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpGet("{UserName}")]
+        public async Task<ActionResult<Admin>> GetAttendance(string UserName)
+        {
+
+            foreach(var admin in _context.Admins)
+            {
+                if(admin.UserName == UserName)
+                {
+                    return admin;
+                }
+            }
+            return NotFound();
+            
+        }
+
     }
 }
