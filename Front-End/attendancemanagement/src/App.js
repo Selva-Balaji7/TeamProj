4d878d39633e6abd447e40914baad343e829409f
natheesh-123 <natheeshkumar4656@gmail.com>
2025-01-16 08:53:57 +0530

    new

--- Front-End/attendancemanagement/src/App.js
+++ Front-End/attendancemanagement/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginComp from '../Layouts/LoginComp';
+import LoginComp from './Layouts/LoginComp';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      {/* {
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -13,12 +14,14 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
+
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header> */}
+      </header>
+      } */}
 
       <LoginComp></LoginComp>
     </div>
