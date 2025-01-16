78fd00b065ccd1b649a58adff90ed58ec71a9235
bejesh-git <bejeshsriram2808@gmail.com>
2025-01-16 17:52:38 +0530

    Attendance comp

--- Front-End/attendancemanagement/src/App.js
+++ Front-End/attendancemanagement/src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import EmployeeAttendanceComp from './Crud/EmployeeAttendanceComp';
+//import logo from './logo.svg';
+//import './App.css';
+import HomeComp from './Layouts/HomeComp';
+import LoginComp from './Layouts/LoginComp';
 
 function App() {
   return (
     <div className="App">
-      <EmployeeAttendanceComp/>
+      {/* <header className="App-header">
+      {/* {
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      {/* <HomeComp></HomeComp>
+      <LoginComp></LoginComp> */}
     </div>
   );
 }
--- Front-End/attendancemanagement/src/index.js
+++ Front-End/attendancemanagement/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider } from 'react-router-dom';
+import routing from './MainRouting/Routing';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={routing}></RouterProvider>
   </React.StrictMode>
 );
 
