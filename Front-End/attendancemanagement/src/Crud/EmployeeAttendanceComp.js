9248f12116e988ee51cbbad4ae57f5dc4ddd755d
bejesh-git <bejeshsriram2808@gmail.com>
2025-01-16 15:37:40 +0530

    Add Attendance

--- Front-End/attendancemanagement/src/Crud/EmployeeAttendanceComp.js
+++ Front-End/attendancemanagement/src/Crud/EmployeeAttendanceComp.js
@@ -1,78 +1,140 @@
-import React, { useState} from 'react'
+import React, { useEffect, useState} from 'react'
+import { GET, POST, PUT } from '../Shared/HttpService';
+import axios from 'axios';
 
 const EmployeeAttendanceComp = () => {
-    const [employee, setEmployee] = useState([
-            {eid:1,ename:"Rahul",Date:"2025-01-16",intime:"09:45",outtime:"06:45"},
-            {eid:1,ename:"John",Date:"2025-01-16",intime:"09:30",outtime:"06:30"},
-        ]);
-        const [attendance, setAttendance] = useState([]);
+
+    let attendanceObj={
+        empId: "",
+        empName:"",
+        inTime:"",
+        outTime:""
+    };
+
+    const [employee, setEmployee] = useState([        ]);
+
+        const [attendance, setAttendance] = useState([        ]);
+
+        const [userempId, setUserempId] = useState(0);
+
         const [error, setError] = useState("");
+
+        let emp = null;
+
+        // /*
+        useEffect(() => {
+            GET("/api/Attendance")
+                .then((res)=>setAttendance(res.data))
+                .catch((error)=>setError("Unable to Fetch Attendance List"+error));
+
+            GET("/api/Employee")
+                .then((res)=>{
+                    setEmployee(res.data);
+                })
+                .catch((error)=>setError("Unable to Fetch Employee List"+error));
+        }
+        ,[]);
+        // */
      
     
-        const handleMarkAttendance = (id) => {
-    
-            const currentDate=new Date().toLocaleDateString();
-            const currentTime=new Date().toLocaleTimeString();
-            console.log(currentDate);
-            console.log(currentTime);
-            const existingRecord = attendance.find(
-                (record) => record.eid === employee.eid && record.date === today
-            );
-           if(existingRecord && existingRecord.intime && !existingRecord.outtime){
-                  existingRecord.outtime=Date().toLocaleTimeString();
-                  setAttendance([...attendance]);
-           }else if(existingRecord && existingRecord.intime && existingRecord.outtime){
-            setError('Attendance for ${employee.ename} is already marked');
-           }
-           
-           else{
-               const newRecord={
-                   eid:employee.eid,
-                   date:today,
-                   intime:Date().toLocaleTimeString(),
-                   outtime:""
-               };
-               setAttendance([...attendance,newRecord]);
-           }
-        }  
+        const handleMarkAttendance = (event) => {
+            // event.preventDefault();
+            const currentTime=new Date().toLocaleTimeString('en-GB', {hour12:false});
+            console.log("Current Time",currentTime);
+            console.log("User EMp Id",userempId);
 
-return(
-    <div>
-         <h2>Attendance Records</h2>
-            {attendance.length === 0 ? (
-                <p>No attendance records found.</p>
-            ) : (
-                <table border="1" style={{ width: '100%', marginTop: '20px' }}>
-                    <thead>
-                        <tr>
-                            <th>Employee ID</th>
-                            <th>Employee Name</th>
-                            <th>Date</th>
-                            <th>Intime</th>
-                            <th>Outtime</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {attendance.map((record, index) => (
-                            <tr key={index}>
-                                <td>{record.eid}</td>
-                                <td>{record.ename}</td>
-                                <td>{record.date}</td>
-                                <td>{record.intime}</td>
-                                <td>{record.outtime || 'N/A'}</td>
+            let attIndex = -1;
+            attendance.map((val, ind) => {
+                if(val.empId == userempId) attIndex = ind;
+            })
+
+            //IF ATTENDANCE NOT FOUND
+            if(attIndex == -1){
+                console.log("IF ATTENDANCE NOT FOUND");
+                emp = employee.filter((val) =>{
+                    return val.empId == userempId;
+                })[0];
+
+                //IF EMPLOYEE NOT FOUND
+                if(emp == null){
+                    console.log("IF EMPLOYEE NOT FOUND");
+                    setError("Unable to find Employee, Contact Admin to Add you");
+                }
+                //IF EMPLOYEE FOUND
+                else{
+                    console.log("IF EMPLOYEE FOUND", {...emp});
+                    attendanceObj = {...emp, inTime:currentTime, outTime:""};
+                    
+                    console.log(attendanceObj);
+                    POST("/api/Attendance", attendanceObj)
+                        .then(()=>{
+                            window.alert("Logged Successfully");
+                        })
+                        .catch(()=>setError("Unable to Log Attendance"));
+                        
+
+                }
+            }
+            //IF ATTENDANCE FOUND
+            else
+            {
+                //IF outTime IS EMPTY
+                if(attendance[attIndex].outTime == null){
+                    attendanceObj = {...attendance[attIndex], outTime:{currentTime}};
+                    PUT(`/api/Attendance/${attendanceObj.empId}`, attendanceObj)
+                        .then(()=>window.alert("Loggout Successfully"))
+                        .catch(()=>setError("Unable to Loggout"));
+                }
+                //IF outTime IS EMPTY
+                else{
+                    window.alert("You have already loggout");
+                }
+            }
+        }
+
+    return(
+        <div>
+            {error && window.alert(error)}     
+
+            <form onSubmit={handleMarkAttendance}>
+                <input type="text" name="userempId" value={userempId} onChange={(e)=>setUserempId(e.target.value)}></input>
+                <button type="submit" className='btn btn-primary '>Mark Attendance</button>  
+            </form>
+
+            <h2>Attendance Records</h2>
+                {attendance.length === 0 ? (
+                    <p>No attendance records found.</p>
+                ) : (
+                    <table border="1" style={{ width: '100%', marginTop: '20px' }}>
+                        <thead>
+                            <tr>
+                                <th>Employee ID</th>
+                                <th>Employee Name</th>
+                                <th>inTime</th>
+                                <th>outTime</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {attendance.map((record, index) => (
+                                <tr key={index}>
+                                    <td>{record.empId}</td>
+                                    <td>{record.empName}</td>
+                                    <td>{record.inTime}</td>
+                                    <td>{record.outTime || 'N/A'}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    
+                )}
+
+            
                 
-            )}
 
-        <button type='button' onClick={() => handleMarkAttendance(val.id)} className='btn btn-primary '>Mark Attendance</button>                    
-              
-
-    </div>
-)
+        </div>
+    )
 
 
 
 }
+export default EmployeeAttendanceComp;
