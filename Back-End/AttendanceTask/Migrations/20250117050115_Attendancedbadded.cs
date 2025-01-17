cc686cc67f9e25c3ff58d12a84b3df4f12846ee6
natheesh <natheeshkumar4656@gmail.com>
2025-01-17 17:59:15 +0530

    new

---
+++ Back-End/AttendanceTask/Migrations/20250117050115_Attendancedbadded.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AttendanceTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class Attendancedbadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "admins",
+                columns: table => new
+                {
+                    userName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    userPassword = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "attendance",
+                columns: table => new
+                {
+                    empId = table.Column<int>(type: "int", nullable: false),
+                    empName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    inTime = table.Column<TimeOnly>(type: "time", nullable: true, defaultValueSql: "(NULL)"),
+                    outTime = table.Column<TimeOnly>(type: "time", nullable: true, defaultValueSql: "(NULL)")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__attendan__AFB3EC0D70B67406", x => x.empId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    empId = table.Column<int>(type: "int", nullable: false),
+                    empName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__employee__AFB3EC0DBACBDA35", x => x.empId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "admins");
+
+            migrationBuilder.DropTable(
+                name: "attendance");
+
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
